1cf66d5e1cd29f2a5ee41a52fc0757cf4768ecbb
patapem <125696143+patapem@users.noreply.github.com>
2026-06-06 19:09:04 +0200

    Create 05-cleanup.sh

---
+++ build_files/recipes/05-cleanup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -ouex pipefail
+
+echo "--- Cleaning up image ---"
+
+# Clean up dnf cache to reduce image size
+dnf5 -y clean all
+rm -rf /run/dnf /run/selinux-policy
+rm -rf /var/lib/dnf
