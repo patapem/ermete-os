88109953e396ad54d9c8ce651a503a535e8847af
patapem <125696143+patapem@users.noreply.github.com>
2026-06-06 19:34:15 +0200

    Update 01-system-setup.sh

--- build_files/recipes/01-system-setup.sh
+++ build_files/recipes/01-system-setup.sh
@@ -2,7 +2,13 @@
 set -ouex pipefail
 
 echo "--- Configuring DNF and installing base system packages ---"
-sed -i '/^\[main\]/a max_parallel_downloads=9' /etc/dnf/dnf.conf
+
+# Approccio Idempotente e Cloud-Native per configurare DNF
+mkdir -p /etc/dnf/dnf.conf.d/
+cat > /etc/dnf/dnf.conf.d/99-parallel-downloads.conf << EOF
+[main]
+max_parallel_downloads=10
+EOF
 
 # System apps
 dnf -y install libvirt virt-manager qemu-kvm flatpak-builder wlr-randr iotop sysstat lxqt-openssh-askpass lxpolkit parallel just seahorse
