a32aa224d684beb8fc0946b419700e65d815de4f
patapem <125696143+patapem@users.noreply.github.com>
2026-06-06 19:35:00 +0200

    Update 03-desktop.sh

--- build_files/recipes/03-desktop.sh
+++ build_files/recipes/03-desktop.sh
@@ -6,8 +6,8 @@ echo "--- Installing Desktop Environment ---"
 # Install Niri 
 dnf -y install niri bibata-cursor-theme
 
-# Install Dank Linux shell
-sudo curl --output-dir "/etc/yum.repos.d/" \
+# Install Dank Linux shell (Senza sudo, siamo già root nel container)
+curl --output-dir "/etc/yum.repos.d/" \
   --remote-name "https://copr.fedorainfracloud.org/coprs/avengemedia/dms/repo/fedora-$(rpm -E %fedora)/avengemedia-dms-fedora-$(rpm -E %fedora).repo"
 
 dnf -y install quickshell dms greetd dms-greeter --allowerasing
