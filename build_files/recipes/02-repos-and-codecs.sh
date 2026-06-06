66340a4defca047c785a15ad2353672cdc4f5e6a
patapem <125696143+patapem@users.noreply.github.com>
2026-06-06 19:08:07 +0200

    Create 02-repos-and-codecs.sh

---
+++ build_files/recipes/02-repos-and-codecs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -ouex pipefail
+
+echo "--- Adding third-party repositories and codecs ---"
+
+# OBS and fully-featured ffmpeg with nonfree components from rpm fusion
+dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf -y install ffmpeg x264-libs obs-studio obs-studio-plugin-x264 libva-utils --allowerasing
+
+# Nautilus open any terminal extension
+curl -Lo /etc/yum.repos.d/nautilus-open-any-terminal.repo \
+  https://copr.fedorainfracloud.org/coprs/monkeygold/nautilus-open-any-terminal/repo/fedora-$(rpm -E %fedora)/monkeygold-nautilus-open-any-terminal-fedora-$(rpm -E %fedora).repo
