7de932ca745e5f63ba1bbd96bbc6c7948b14013f
patapem <125696143+patapem@users.noreply.github.com>
2026-06-08 12:45:48 +0200

    Update 01-system-setup.sh

--- build_files/recipes/01-system-setup.sh
+++ build_files/recipes/01-system-setup.sh
@@ -10,8 +10,12 @@ cat > /etc/dnf/dnf.conf.d/99-parallel-downloads.conf << EOF
 max_parallel_downloads=10
 EOF
 
-# System apps
-dnf -y install libvirt virt-manager qemu-kvm flatpak-builder wlr-randr iotop sysstat lxqt-openssh-askpass lxpolkit parallel just seahorse
+# System apps & Dipendenze Core
+dnf -y install libvirt virt-manager qemu-kvm flatpak-builder wlr-randr sysstat lxqt-openssh-askpass lxpolkit parallel just seahorse
+dnf -y install swaylock # Dipendenza critica per il blocco schermo di Niri
 
-# User apps
-dnf -y install nautilus kitty mpv gnome-terminal gnome-system-monitor gnome-calculator
+# Core Utilities in Rust (Il nuovo stack)
+dnf -y install eza bat fd-find ripgrep bottom nushell starship
+
+# User apps (Rust focused)
+dnf -y install nautilus alacritty mpv
