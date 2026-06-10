422bbd64fe7d40dd9e6a1d97f6ada85e5d23f078
Ermeteos Developer <dev@ermeteos.local>
2026-06-10 21:40:51 +0200

    refactor: build anyrun and ironbar natively from crates.io to avoid unreliable coprs

--- build_files/recipes/03-desktop.sh
+++ build_files/recipes/03-desktop.sh
@@ -9,32 +9,19 @@ dnf -y install --setopt=install_weak_deps=False niri bibata-cursor-theme
 # Install Greetd e Tuigreet (Greeter da terminale in Rust)
 dnf -y install --setopt=install_weak_deps=False greetd tuigreet
 
-# Anyrun: App Launcher iper-veloce in Rust
-cat > /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:reisaraujo-miguel:Anyrun.repo << 'EOF'
-[copr:copr.fedorainfracloud.org:reisaraujo-miguel:Anyrun]
-name=Copr repo for Anyrun owned by reisaraujo-miguel
-baseurl=https://download.copr.fedorainfracloud.org/results/reisaraujo-miguel/Anyrun/fedora-43-$basearch/
-type=rpm-md
-skip_if_unavailable=True
-gpgcheck=1
-gpgkey=https://download.copr.fedorainfracloud.org/results/reisaraujo-miguel/Anyrun/pubkey.gpg
-repo_gpgcheck=0
-enabled=1
-enabled_metadata=1
-EOF
-dnf -y install --setopt=install_weak_deps=False Anyrun
+# Compilazione nativa in Rust per Anyrun e Ironbar
+# Evitiamo repository COPR inaffidabili o non aggiornati per Rawhide (fc43).
+# Installiamo i tool di compilazione e le dipendenze GTK/Wayland necessarie.
+dnf -y install --setopt=install_weak_deps=False rust cargo gcc gcc-c++ pkgconf-pkg-config glib2-devel gtk3-devel gtk-layer-shell-devel cairo-devel pango-devel gdk-pixbuf2-devel
 
-# Ironbar: Status bar moderna e configurabile scritta in Rust (forzato su fedora-42)
-cat > /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:deluxe-cube:ironbar.repo << 'EOF'
-[copr:copr.fedorainfracloud.org:deluxe-cube:ironbar]
-name=Copr repo for ironbar owned by deluxe-cube
-baseurl=https://download.copr.fedorainfracloud.org/results/deluxe-cube/ironbar/fedora-42-$basearch/
-type=rpm-md
-skip_if_unavailable=True
-gpgcheck=1
-gpgkey=https://download.copr.fedorainfracloud.org/results/deluxe-cube/ironbar/pubkey.gpg
-repo_gpgcheck=0
-enabled=1
-enabled_metadata=1
-EOF
-dnf -y install --setopt=install_weak_deps=False ironbar
+# Compilazione e installazione
+cargo install anyrun
+cargo install ironbar
+
+# Sposta i binari in una directory di sistema globale
+mv /root/.cargo/bin/anyrun /usr/bin/
+mv /root/.cargo/bin/ironbar /usr/bin/
+
+# Pulizia: rimuoviamo i tool di build per non appesantire l'immagine OCI atomica
+dnf -y remove rust cargo gcc gcc-c++ pkgconf-pkg-config glib2-devel gtk3-devel gtk-layer-shell-devel cairo-devel pango-devel gdk-pixbuf2-devel
+rm -rf /root/.cargo
