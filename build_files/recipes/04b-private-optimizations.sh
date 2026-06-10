9acde4f3b53c4aa411f3f6b237f27cd0fd3f81b0
Ermeteos Developer <dev@ermeteos.local>
2026-06-10 23:31:13 +0200

    feat: add flatpak warehouse to first boot setup

--- build_files/recipes/04b-private-optimizations.sh
+++ build_files/recipes/04b-private-optimizations.sh
@@ -54,7 +54,7 @@ ExecStartPre=-/usr/bin/flatpak remote-add --if-not-exists flathub https://dl.fla
 ExecStartPre=-/usr/bin/flatpak override --system --env=GTK_THEME=adw-gtk3-dark
 ExecStartPre=-/usr/bin/flatpak override --system --env=ICON_THEME=Papirus-Dark
 # Installa tutte le GUI Application essenziali via Flatpak per preservare la purezza del RootFS
-ExecStart=-/usr/bin/flatpak install -y flathub com.github.tchx84.Flatseal org.gnome.Nautilus org.alacritty.Alacritty io.mpv.Mpv com.obsproject.Studio
+ExecStart=-/usr/bin/flatpak install -y flathub io.github.flattool.Warehouse com.github.tchx84.Flatseal org.gnome.Nautilus org.alacritty.Alacritty io.mpv.Mpv com.obsproject.Studio
 RemainAfterExit=yes
 
 [Install]
